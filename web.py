10f295615d598dd8a9430c6bed000d4a071a455e
pekka <pekkaj.kostiainen@gmail.com>
2023-09-14 20:12:29 +0300

    Initial commit

---
+++ web.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    print(todo)
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("must todo app")
+st.subheader("this must be todo")
+st.write("nostaako tuottavuutta toimintaa")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
+st.text_input(label="", placeholder="Add new todo: ",
+              on_change=add_todo, key='new_todo')
+
+
